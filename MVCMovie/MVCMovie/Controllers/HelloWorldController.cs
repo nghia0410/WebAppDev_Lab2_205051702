b2a0b0f05bd5b1886ec4a9f97ceefbc0bf963f5b
BuiHoangNghia <nghiabh20@uef.edu.vn>
2023-03-05 13:37:26 +0700

    first commit

---
+++ MVCMovie/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+
+    //public IActionResult Index()
+    //{
+    // return View();
+    //}
+    //
+    // GET: /HelloWorld/
+
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        //
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+    }
+}
